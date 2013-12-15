067784a48d18f62b84c39118999aefc647657db9
Dennis Lutter <lad1337@gmail.com>
2013-12-15 19:31:48 +0100

    new XEMNames

---
+++ XEMNames/xemnames.py
@@ -0,0 +1,76 @@
+# Author: Dennis Lutter <lad1337@gmail.com>
+# URL: https://github.com/lad1337/XDM
+#
+# This file is part of XDM: eXtentable Download Manager.
+#
+# XDM: eXtentable Download Manager. Plugin based media collection manager.
+# Copyright (C) 2013  Dennis Lutter
+#
+# XDM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# XDM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+import os
+from xdm.plugins import *
+from datetime import datetime, timedelta
+from lib import requests
+import json
+from lib.dateutil import parser
+
+my_install_folder = os.path.dirname(__file__)
+
+class XEMNames(SearchTermFilter):
+    version = "0.1"
+    identifier = "de.lad1337.xem.names"
+    addMediaTypeOptions = 'runFor'
+    config_meta = {'plugin_desc': 'Gets additional names for TV shows from http://thexem.de',
+                  }
+    _hidden_config = {'last_cache': ''}
+    def compare(self, element, terms):
+        if element.type != "Episode":
+            log("i only work for Episodes, i got a %s" % element.type)
+            return terms
+        show = element.parent.parent
+        tvdb_id = str(show.getField('id', 'tvdb'))
+        if not tvdb_id:
+            log("no tvdb_id found for %s" % show)
+            return terms
+        names = self._getNames()
+        if tvdb_id in names:
+            log("found the show by tvdb id on xem")
+            new_names = []
+            for xem_name_data in names[tvdb_id]:
+                for xem_name, season in xem_name_data.items():
+                    if element.parent.number == season or season == -1:
+                        data = {"show_name": xem_name,
+                                "s#": element.parent.number,
+                                "e#": element.number
+                                }
+                        cur_name = u"{show_name} s{s#:0>2}e{e#:0>2}".format(**data)
+                        new_names.append(cur_name)
+            terms = new_names + terms
+        return terms
+
+    def _getNames(self):
+        if not self.hc.last_cache or parser.parse(self.hc.last_cache) < datetime.now() - timedelta(days=2):
+            log("getting new names from xem")
+            r = requests.get("http://thexem.de/map/allNames?origin=tvdb&seasonNumbers=1")
+            names = r.json()["data"]
+            cache_file_path = os.path.join(my_install_folder, 'cache.json')
+            log("saving xem names to %s" % cache_file_path)
+            with open(cache_file_path, "w") as cache:
+                cache.write(json.dumps(names))
+            self.hc.last_cache = str(datetime.now())
+
+        with open(os.path.join(my_install_folder, 'cache.json'), "r") as cache:
+            out = json.loads(cache.read())
+        return out
